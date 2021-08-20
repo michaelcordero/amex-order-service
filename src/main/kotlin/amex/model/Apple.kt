3669dc68cf2983f2b9d2b4b1d92a01a806a3d19d
michaelcordero <michaelpetercordero@gmail.com>
2021-08-20 16:11:53 -0400

    deleted unused objects

--- src/main/kotlin/amex/model/Apple.kt
+++
@@ -1,8 +0,0 @@
-package amex.model
-
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Apple @JsonCreator constructor(
-    @JsonProperty(value = "apples", required = true)
-    val number: Int)
--- src/main/kotlin/amex/model/Orange.kt
+++
@@ -1,8 +0,0 @@
-package amex.model
-
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Orange @JsonCreator constructor(
-    @JsonProperty(value = "oranges", required = true)
-    val number: Int)
