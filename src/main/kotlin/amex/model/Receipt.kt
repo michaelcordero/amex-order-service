c4d82f578e7d8d542012f32f9cf591e34c8d5dd9
michaelcordero <michaelpetercordero@gmail.com>
2021-08-20 16:36:15 -0400

    Step Two: Complete

--- src/main/kotlin/amex/model/Receipt.kt
+++ src/main/kotlin/amex/model/Receipt.kt
@@ -10,8 +10,11 @@ data class Receipt @JsonCreator constructor(
     @JsonProperty(value = "oranges", required = false)
     val oranges: Int?) : Serializable {
     fun total(): Double {
-        val applesCost: Double = apples?.times(0.60) ?: 0.0
-        val orangesCost: Double = oranges?.times(0.25) ?: 0.0
+        val num_of_apples: Int = apples ?: 0
+        val num_of_oranges: Int = oranges ?: 0
+        val remainder: Int = num_of_oranges % 3
+        val applesCost: Double =  (num_of_apples / 2) * 0.60
+        val orangesCost: Double = (((num_of_oranges / 3) * 2) + remainder) * 0.25
         return applesCost + orangesCost
     }
 
