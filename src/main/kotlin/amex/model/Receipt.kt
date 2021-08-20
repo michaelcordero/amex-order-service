c0a6d2fac597e9116bc6759f67efc954468ec80d
michaelcordero <michaelpetercordero@gmail.com>
2021-08-20 19:38:11 -0400

    last minute math corrections

--- src/main/kotlin/amex/model/Receipt.kt
+++ src/main/kotlin/amex/model/Receipt.kt
@@ -3,6 +3,8 @@ package amex.model
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.io.Serializable
+import kotlin.math.ceil
+import kotlin.math.roundToInt
 
 data class Receipt @JsonCreator constructor(
     @JsonProperty(value = "apples", required = false)
@@ -13,7 +15,7 @@ data class Receipt @JsonCreator constructor(
         val num_of_apples: Int = apples ?: 0
         val num_of_oranges: Int = oranges ?: 0
         val remainder: Int = num_of_oranges % 3
-        val applesCost: Double =  (num_of_apples / 2) * 0.60
+        val applesCost: Double =  ceil(num_of_apples.toDouble() / 2.0).roundToInt() * 0.60
         val orangesCost: Double = (((num_of_oranges / 3) * 2) + remainder) * 0.25
         return applesCost + orangesCost
     }
