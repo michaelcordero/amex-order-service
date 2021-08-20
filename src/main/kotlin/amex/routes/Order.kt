f42421af8a42143e546e6192513466ea2ea2c840
michaelcordero <michaelpetercordero@gmail.com>
2021-08-20 18:05:19 -0400

    Step Three: Working Solution (now to ensure that this will work on other machines)

--- src/main/kotlin/amex/routes/Order.kt
+++ src/main/kotlin/amex/routes/Order.kt
@@ -1,7 +1,9 @@
 package amex.routes
 
+import amex.db
 import amex.log
 import amex.model.Receipt
+import amex.order_counter
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.locations.*
@@ -11,18 +13,56 @@ import io.ktor.routing.*
 
 @KtorExperimentalLocationsAPI
 @Location("/order")
-class Order
+class PostOrder {
+    @Location("/{id}")
+    data class Order(val id: String?, val postOrder: PostOrder)
+    @Location("/all")
+    class All(val postOrder: PostOrder)
+}
 
 @KtorExperimentalLocationsAPI
 fun Route.order() {
-    post<Order> {
+    // Create
+    post<PostOrder> {
         try {
             log.info("POST /order requested")
             val receipt: Receipt = call.receive()
-            call.respond(status = HttpStatusCode.OK, message = receipt.summary())
+            // save it to "database"
+            order_counter += 1
+            db[order_counter] = receipt
+            call.respond(status = HttpStatusCode.OK, message = receipt.summary(0))
         } catch (e: Exception) {
             log.error(e.message)
             call.respond(status = HttpStatusCode.BadRequest, message = mapOf("error" to e.toString()))
         }
     }
+
+    // Read
+    get<PostOrder.Order> {
+        try {
+            log.info("GET /order/{id} requested")
+            var receipt: Receipt? = Receipt(0,0)
+            if (it.id != null) {
+                receipt = db.get(it.id.toInt())
+            }
+            if (receipt != null) {
+                call.respond(status = HttpStatusCode.OK, message = receipt.summary(it.id?.toInt()))
+            }
+        } catch (e: Exception) {
+            log.error(e.message)
+            call.respond(status = HttpStatusCode.BadRequest, message = mapOf("error" to e.toString()))
+        }
+
+    }
+
+    get<PostOrder.All> {
+        try {
+            log.info("GET /order/all requested")
+            call.respond(status = HttpStatusCode.OK, message = db.entries.map { entry -> entry.value.summary(entry.key) }.toString())
+        } catch (e: Exception) {
+            log.error(e.message)
+            call.respond(status = HttpStatusCode.BadRequest, message = mapOf("error" to e.toString()))
+        }
+    }
+
 }
